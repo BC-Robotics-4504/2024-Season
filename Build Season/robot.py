591e2c44d1368cec383e1e1b89c5ac42242a6ca6
Dan Sweeney <sweeneyd@users.noreply.github.com>
2024-01-06 13:06:54 -0500

    Update robot.py

--- Build Season/robot.py
+++ Build Season/robot.py
@@ -1,3 +1,26 @@
-from subsystems import climber_system as climber
-from subsystems import drivetrain_system as drivetrain
-from subsystems import launcher_system as launcher
+from subsystems import climber_system, drivetrain_system, launcher_system
+
+from magicbot import MagicRobot
+from networktables import NetworkTables
+
+class MyRobot(MagicRobot):
+    # Create low-level object
+    drivetrain: drivetrain_subsystem
+    launcher: launcher_subsystem
+    climber: climber_subsystem
+
+    def createObjects(self):
+      return None
+
+    def disabledPeriodic(self):
+      return None
+
+    def teleopInit(self):
+      return None
+
+    def teleopPeriodic(self):
+      return None
+
+
+if __name__ == "__main__":
+    wpilib.run(MyRobot)
